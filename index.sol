7dc9069a09ad7d31da496df186209b3ccd5b790b
Dharti Parikh <dhartiparikh@Dhartis-MacBook-Air.local>
2019-05-25 21:30:47 -0400

    my first commit

---
+++ index.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.5.8;
+
+contract SimpleBank {
+    struct clientinfo{
+        string name;
+        uint age;
+        address cli; 
+    }
+    uint private clientCount;
+    mapping (address => uint) private balances;
+    
+   clientinfo[]  client;
+   
+    address public owner;
+    
+    constructor() public  {
+        
+        owner = msg.sender;
+      clientCount=0;
+    }
